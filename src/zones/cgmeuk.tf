01c7e1fb9bbe20a62ef18441c9faa3acc324a02f
Connor Gurney <me@connorgurney.me.uk>
2024-08-01 09:44:31 +0100

    Add records for connorgurney.me.uk This is still in Cloudflare right now for reasons that are far too dull to explain here, but is due to move over soon.

--- src/zones/cgmeuk.tf
+++ src/zones/cgmeuk.tf
@@ -5,3 +5,76 @@ resource "aws_route53_zone" "cgmeuk" {
     "connorgurney:workload" = "cgmeuk"
   }
 }
+
+# Root TXT records
+# I do this here as Amazon Route 53 doesn’t support having more than one TXT record at the root of the zone.
+resource "aws_route53_record" "cgmeuk_root_txt" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = "3600"
+  records = [
+    # Email SPF
+    "v=spf1 include:icloud.com -all",
+
+    # Apple domain verification
+    "apple-domain=bspmYxr24yNNDCpD"
+  ]
+}
+
+# Root record
+resource "aws_route53_record" "cgmeuk_root" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = ""
+  type    = "CNAME"
+  ttl     = "3600"
+  records = [
+    "d36bj2gmhc6qn1.cloudfront.net"
+  ]
+}
+
+# WWW record
+resource "aws_route53_record" "cgmeuk_www" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = [
+    "d36bj2gmhc6qn1.cloudfront.net"
+  ]
+}
+
+# Email routing
+resource "aws_route53_record" "cgmeuk_mx" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = ""
+  type    = "MX"
+  ttl     = "3600"
+  records = [
+    "10 mx01.mail.icloud.com",
+    "10 mx02.mail.icloud.com"
+  ]
+}
+
+# Email DMARC
+# This is set as it is as I don’t send email from this domain.
+resource "aws_route53_record" "cgmeuk_dmarc" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = "_dmarc"
+  type    = "TXT"
+  ttl     = "3600"
+  records = [
+    "v=DMARC1; p=reject; adkim=s; aspf=s; fo=1; rua=mailto:postmaster@connorgurney.me.uk; ruf=mailto:postmaster@connorgurney.me.uk"
+  ]
+}
+
+# Email DKIM
+resource "aws_route53_record" "cgmeuk_dkim" {
+  zone_id = aws_route53_zone.cgmeuk.zone_id
+  name    = "sig1._domainkey"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = [
+    "sig1.dkim.connorgurney.me.uk.at.icloudmailadmin.com"
+  ]
+}
